9c0e07f2d1dde18e8165724499fc355d7fa12c7a
Douglas Townsend <dougymt@gmail.com>
2024-02-07 08:33:25 -0500

    nav is a seperate component

--- client/app.vue
+++ client/app.vue
@@ -1,73 +1,38 @@
 <template>
-  <body class="background-clr">
-
-    <nav class="nav-bar">
-      <h1>LOCK N' ROLL PROMOTIONS</h1>
-
-      <ul class="main-menu">
-        <li><a href="">Home</a></li>
-        <li><a href="">Buy Tickets</a></li>
-        <li><a href="">Upcoming Seminars</a></li>
-        <li><a href="">Contact</a></li>
-
-        <!-- <li><button id="tickets-button" class="nav-btn" >Buy Tickets</button></li> -->
-      </ul>
-    </nav>
-
-    <div class="seminar-img">
-      a
-    </div>
-
+  <main class="app__main">
+    <NavBar />
+    <div class="seminar-img">a</div>
     <footer class="footer">
-      <div class="footer-container">
-        footer n shieeeeet
-      </div>
+      <div class="footer-container">footer n shieeeeet</div>
     </footer>
-
-  </body>
+  </main>
 </template>
 
 <style>
-  .seminar-img {
-    background-color: white;
-    height: 50vh;
-    width: 50vh;
-    margin: auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .footer-container {
-    background-color: white;
-    width: 100%;
-    height: 10%;
-    position: absolute;
-    bottom: 0;
-  }
-  .background-clr {
-    background-color: #191923
-  }
-  .nav-bar {
-    background-color: white;
-  }
-  .main-menu {
-    list-style-type: none;
-    padding: 0px;
-    margin: 0px;
-    overflow: hidden;
-  }
-  .main-menu a{
-    text-decoration: none;
-    color: black;
-    display: block;
-    text-align: center;
-    padding: 15px;
-  }
-  .main-menu a:hover{
-    background-color: #E1DFF6
-  }
-  .main-menu li{
-    float: left;
-  }
-</style>
+.app__main {
+  --black_main: #191923;
+  --white_main: #f3f3fe;
+  background-color: var(--black_main);
+  font-size: 16px;
+}
 
+.seminar-img,
+.footer-container {
+  background-color: var(--white_main);
+}
+
+.seminar-img {
+  min-height: min(50vh, 500px);
+  width: 50%;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.footer-container {
+  width: 100%;
+  height: 10%;
+  position: absolute;
+  bottom: 0;
+}
+</style>
---
+++ client/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="nav-bar">
+    <h1>LOCK N' ROLL PROMOTIONS</h1>
+    <ul class="nav-bar__content">
+      <NavItem v-for="link in links" :navLink="link" />
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+const links = [
+  {
+    href: "gjgj",
+    text: "Home",
+  },
+  {
+    href: "bhj",
+    text: "Buy Tickets",
+  },
+  {
+    href: "bjkb",
+    text: "Upcoming Seminars",
+  },
+  {
+    href: "kjhk",
+    text: "Contact",
+  },
+];
+</script>
+
+<style scoped>
+.nav-bar {
+  background-color: var(--white_main);
+}
+
+.nav-bar__content {
+  list-style-type: none;
+  padding: 0px;
+  margin: 0px;
+  overflow: hidden;
+}
+</style>
