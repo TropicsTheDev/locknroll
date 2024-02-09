85f167d04e46d0653e2d5d5c3fbb2f1ce4a003db
Douglas Townsend <dougymt@gmail.com>
2024-02-09 09:19:42 -0500

    font change to Hack for consistency. Font kit added to project

--- client/app.vue
+++ client/app.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="app__main">
     <header class="app__header">
-      <h1>Deez Nuts</h1>
+      <h1>Lock N' Roll Promotions</h1>
       <NavBar />
     </header>
     <section class="featured">
@@ -36,33 +36,32 @@
       <h2 class="past-events__heading">Past Events</h2>
       <main class="past-events__body">
         <aside class="past-events__img-wrapper">
-        <img
-          class="past-events__img fill-container"
-          src="assets/styles/img/untitled1.png"
-          alt="cinematic picture of grappling"
-        />
-      </aside>
+          <img
+            class="past-events__img fill-container"
+            src="assets/styles/img/untitled1.png"
+            alt="cinematic picture of grappling"
+          />
+        </aside>
         <article class="past-events__content">
-        <h3>Dante My Beloved</h3>
-        <h4>No-Gi Jiu Jitsu Seminar</h4>
-        <p>
-          Lorem ipsum I love Dante Leon, consectetur adipiscing elit. Maecenas
-          urna nibh, sagittis a scelerisque in, tempor mattis arcu.
-          Pellentesque a auctor lacus. Integer lobortis molestie nulla in
-          mattis. Maecenas faucibus mi vel orci aliquam vehicula. Vivamus
-          pulvinar tempor mauris, ut vestibulum erat placerat sed. Mauris sit
-          amet orci est. Donec condimentum ut nibh sed fringilla. Aliquam
-          luctus ultricies semper. Mauris elementum, libero ac hendrerit
-          molestie, sapien sem egestas dui, vel pharetra lectus odio id ex.
-          Nam ultrices consequat luctus. Nulla auctor sem ut est dignissim, in
-          accumsan sapien congue. In vel ultrices odio, sed facilisis augue.
-        </p>
-      </article>
+          <h3>Dante My Beloved</h3>
+          <h4>No-Gi Jiu Jitsu Seminar</h4>
+          <p>
+            Lorem ipsum I love Dante Leon, consectetur adipiscing elit. Maecenas
+            urna nibh, sagittis a scelerisque in, tempor mattis arcu.
+            Pellentesque a auctor lacus. Integer lobortis molestie nulla in
+            mattis. Maecenas faucibus mi vel orci aliquam vehicula. Vivamus
+            pulvinar tempor mauris, ut vestibulum erat placerat sed. Mauris sit
+            amet orci est. Donec condimentum ut nibh sed fringilla. Aliquam
+            luctus ultricies semper. Mauris elementum, libero ac hendrerit
+            molestie, sapien sem egestas dui, vel pharetra lectus odio id ex.
+            Nam ultrices consequat luctus. Nulla auctor sem ut est dignissim, in
+            accumsan sapien congue. In vel ultrices odio, sed facilisis augue.
+          </p>
+        </article>
       </main>
     </section>
     <section class="about">
-      <div class="about__img-wrapper">
-      </div>
+      <div class="about__img-wrapper"></div>
     </section>
   </main>
 </template>
@@ -94,21 +93,24 @@ import "assets/styles/css/main.css";
   padding: 0 var(--f-size-3) var(--f-size-3) var(--f-size-3);
 }
 
-.featured__heading, .past-events__heading {
+.featured__heading,
+.past-events__heading {
   /* Font color and aligning headings to center */
   color: var(--white_main);
   padding: var(--f-size-3);
   text-align: center;
 }
 
-.featured__body, .past-events__body {
+.featured__body,
+.past-events__body {
   /* Centering */
   display: flex;
   align-items: center;
   gap: var(--f-size-base);
 }
 
-.featured__content, .past-events__content {
+.featured__content,
+.past-events__content {
   /* Setting text requirements */
   color: var(--white_main);
   padding: var(--f-size-base);
@@ -116,17 +118,20 @@ import "assets/styles/css/main.css";
   max-width: 50%;
 }
 
-.featured__content h3, .past-events__content h3 {
+.featured__content h3,
+.past-events__content h3 {
   /* Spacing between texts */
   margin-bottom: calc(var(--f-size-base) / 2);
 }
 
-.featured__img-wrapper, .past-events__img-wrapper {
+.featured__img-wrapper,
+.past-events__img-wrapper {
   /* Setting limitations on image size */
   max-width: 640px;
 }
 
-.featured__img, .past-events__img {
+.featured__img,
+.past-events__img {
   object-fit: contain;
 }
 
